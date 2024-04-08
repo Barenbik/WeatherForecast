b45be0a209daf02874fa56dd77b7ac536eb4e84e
Tony Sharples <tonysharples22@me.com>
2024-04-08 16:04:17 +0100

    Create DayForecast view

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 20, low: 11)
+            DayForecast(day: "Tue", isRainy: true, high: 12, low: 5)
+            DayForecast(day: "Wed", isRainy: true, high: 10, low: 6)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            "cloud.rain.fill"
+        } else {
+            "sun.max.fill"
+        }
+    }
+    
+    var iconColour: Color {
+        if isRainy {
+            Color.blue
+        } else {
+            Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            
+            Image(systemName: iconName)
+                .foregroundStyle(iconColour)
+                .font(.largeTitle)
+                .padding(5)
+            
+            Text("High: \(high)")
+                .fontWeight(.bold)
+            
+            Text("Low: \(low)")
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
 }
