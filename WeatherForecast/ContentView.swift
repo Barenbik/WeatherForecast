c84e3fcafc11cf69b7be8affb6402abd0be365bd
Tony Sharples <tonysharples22@me.com>
2024-04-18 16:29:47 +0100

    Create WeekForecast

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let averageHighTemp = (20 + 12 + 10 + 20 + 12 + 10 + 20) / 7
+    let averageLowTemp = (11 + 5 + 6 + 11 + 5 + 6 + 11) / 7
+    
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                DayForecast(day: "Mon", isRainy: false, high: 20, low: 11)
-                DayForecast(day: "Tue", isRainy: true, high: 12, low: 5)
-                DayForecast(day: "Wed", isRainy: true, high: 10, low: 6)
-                DayForecast(day: "Thu", isRainy: false, high: 20, low: 11)
-                DayForecast(day: "Fri", isRainy: true, high: 12, low: 5)
-                DayForecast(day: "Sat", isRainy: false, high: 10, low: 6)
-                DayForecast(day: "Sun", isRainy: false, high: 20, low: 11)
+        VStack {
+            Text("Weekly Forecast")
+                .font(.title)
+                .padding(.horizontal)
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    DayForecast(day: "Mon", isRainy: false, high: 20, low: 11)
+                    DayForecast(day: "Tue", isRainy: true, high: 12, low: 5)
+                    DayForecast(day: "Wed", isRainy: true, high: 10, low: 6)
+                    DayForecast(day: "Thu", isRainy: false, high: 20, low: 11)
+                    DayForecast(day: "Fri", isRainy: true, high: 12, low: 5)
+                    DayForecast(day: "Sat", isRainy: false, high: 10, low: 6)
+                    DayForecast(day: "Sun", isRainy: false, high: 20, low: 11)
+                }
             }
+            
+            Text("Weekly Stats")
+                .font(.title)
+                .padding()
+            
+            WeekForecast(averageHighTemp: averageHighTemp, averageLowTemp: averageLowTemp, numberOfSunnyDays: 4, numberOfRainyDays: 3)
+            
+            Spacer()
         }
     }
 }
@@ -67,5 +84,36 @@ struct DayForecast: View {
                 .foregroundStyle(.secondary)
         }
         .padding()
+    }
+}
+
+struct WeekForecast: View {
+    let averageHighTemp: Int
+    let averageLowTemp: Int
+    let numberOfSunnyDays: Int
+    let numberOfRainyDays: Int
+    
+    var body: some View {
+        VStack {
+            Text("Mon, 15 April 2024 - Sun, 21 April 2024")
+                .padding(.bottom)
+            
+            Text("Average high temperature")
+            Text("\(averageHighTemp)")
+                .padding(.bottom)
+            
+            Text("Average low temperature")
+            Text("\(averageLowTemp)")
+                .padding(.bottom)
+            
+            Text("Number of days with rain")
+            Text("\(numberOfRainyDays)")
+                .padding(.bottom)
+            
+            Text("Number of days with sun")
+            
+            Text("\(numberOfSunnyDays)")
+                .padding(.bottom)
+        }
     }
 }
